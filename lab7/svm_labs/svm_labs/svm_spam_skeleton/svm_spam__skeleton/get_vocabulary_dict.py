cdcbcbeca3a6e231f6606de7206ee528fc6f618d
rafal <rafalsiniewicz@gmail.com>
2020-12-11 22:46:43 +0100

    update

---
+++ lab7/svm_labs/svm_labs/svm_spam_skeleton/svm_spam__skeleton/get_vocabulary_dict.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+
+from typing import Dict
+
+
+def get_vocabulary_dict() -> Dict[int, str]:
+    """Read the fixed vocabulary list from the datafile and return.
+
+    :return: a dictionary of words mapped to their indexes
+    """
+
+    # FIXME: Parse data from the 'data/vocab.txt' file.
+    # - The file is saved in tab-separated values (TSV) format.
+    # - Each line contains a word's ID and the word itself.
+    # The output dictionary should map word's ID on the word itself, e.g.:
+    #   {1: 'aa', 2: 'ab', ...}
+    d = dict()
+    f = open('data/vocab.txt', 'r') 
+    for line in f:
+    	d[int(line.split()[0])] = line.split()[1]
+
+    return d
+
+    # raise NotImplementedError()
